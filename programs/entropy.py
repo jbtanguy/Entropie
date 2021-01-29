e21cfcc17bc8e74f7e01a67969b1a1d1e2ada2ef
jbtanguy <jbtanguy56@gmail.com>
2021-01-29 11:53:06 +0100

    optimisation de la procedure

--- programs/entropy.py
+++ programs/entropy.py
@@ -8,45 +8,31 @@ reference_path = '/home/jbtanguy/These/Corpus/Daniel/Extractions_corpus_daniel/c
 nmin, nmax = (2, 4)
 cleaned_dir_path = '/home/jbtanguy/These/Corpus/Daniel/Extractions_corpus_daniel/corpora/Corpus_daniel_v2.1/cleaned/'
 engines = ['TRAFFallbackComments', 'TRAFFallback', 'TRAFComments', 'TRAF_BL', 'TRAF', 'READ_py', 'READABILITY', 'NEWSPLEASE', 'NEWSPAPER', 'JT_trueLg', 'JT_langid', 'JT_english', 'JT', 'INSCRIPTIS', 'HTML-text', 'HTML2TEXT', 'GOO', 'BP3Largest', 'BP3KeepEverything', 'BP3Article', 'BP3']
+"""REFERENCE
+cleaned_dir_path = '/home/jbtanguy/These/Corpus/Daniel/Extractions_corpus_daniel/corpora/Corpus_daniel_v2.1/'
+engines = ['reference']
+"""
 # 1. Parsing du json 
 languages = json.load(open(doc_lang_path))
 lang = set(languages.values())
 # 2. Chargement du corpus
 for engine in engines:
+	print(engine)
 	corpus_path = cleaned_dir_path + engine + '/'
 	corpus_ref = DanielCorpus(corpus_path, languages)
-# 3. Apprentissage des modèles de langue et sauvegarde
-"""REFERENCE
-LMs = {}
-for l in lang:
-	language_model = corpus_ref.get_language_model(l, nmin=nmin, nmax=nmax)
-	file = open('./../language_models/proba_condi/reference'+'_'+l+'_LM.json', 'w')
-	json.dump(language_model, file, indent=4)
-	LMs[l] = language_model
-"""
+	# 3. Apprentissage des modèles de langue et sauvegarde
 	LMs = {}
 	for l in lang:
 		language_model = corpus_ref.get_language_model(l, nmin=nmin, nmax=nmax)
 		file = open('./../language_models/proba_condi/'+engine+'_'+l+'_LM.json', 'w')
 		json.dump(language_model, file, indent=4)
 		LMs[l] = language_model
-# 4. Calcul de l'entropie
-"""REFERENCE
-entropies = {}
-for l in lang:
-	for doc in corpus_ref.part_lang_[l]:
-		txt = doc.text_
-		path = doc.path_
-		H = entropy(txt, LMs[l], nmin, nmax)
-		entropies[path] = H
-file = open('./../entropies/entropies_reference.json', 'w')
-json.dump(entropies, file, indent=4)
-"""
+	# 4. Calcul de l'entropie
 	entropies = {}
 	for l in lang:
 		for doc in corpus_ref.part_lang_[l]:
 			txt = doc.text_
-			path = doc.path_
+			path = doc.path_.split('/')[-1]
 			H = entropy(txt, LMs[l], nmin, nmax)
 			entropies[path] = H
 	file = open('./../entropies/entropies_'+engine+'.json', 'w')
